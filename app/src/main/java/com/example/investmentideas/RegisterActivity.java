646700455acf615dc31d6af2336a9ed61d22b946
Rasmus Jannerup <rasm211d@edu.easj.dk>
2020-04-23 14:39:10 +0200

    Added some functionality to the RegisterActivity class

--- app/src/main/java/com/example/investmentideas/RegisterActivity.java
+++ app/src/main/java/com/example/investmentideas/RegisterActivity.java
@@ -1,5 +1,33 @@
 package com.example.investmentideas;
 
-public class RegisterActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
     LoginDatabaseHelper db;
+    EditText e1, e2, e3;
+    Button b1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registeractivity);
+        e1 = findViewById(R.id.remail);
+        e2 = findViewById(R.id.rpassword);
+        e3 = findViewById(R.id.rcpassword);
+        b1 = findViewById(R.id.rregister);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = e1.getText().toString();
+                String password = e2.getText().toString();
+                String confirmPassword = e3.getText().toString();
+                
+            }
+        });
+    }
 }
