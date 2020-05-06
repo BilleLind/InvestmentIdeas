4e3b43000a180564de8b5979310f7040ac53dc5c
yokarak <54890038+yokarak@users.noreply.github.com>
2020-05-06 22:24:04 +0200

    barChart works, added some styling more later

--- app/src/main/java/com/example/investmentideas/ExploreAcitivty.java
+++ app/src/main/java/com/example/investmentideas/ExploreAcitivty.java
@@ -1,5 +1,6 @@
 package com.example.investmentideas;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -13,8 +14,13 @@ import androidx.core.content.ContextCompat;
 import androidx.core.view.GestureDetectorCompat;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class ExploreAcitivty extends AppCompatActivity{
@@ -37,7 +43,8 @@ public class ExploreAcitivty extends AppCompatActivity{
 
     private GestureDetectorCompat gestureDetectorCompat = null;
 
-    //BarChart barChart;
+
+
 
 
     @Override
@@ -82,9 +89,92 @@ public class ExploreAcitivty extends AppCompatActivity{
             }
         });
 
+        barChartMakerMethod();
+
+    }
+    public void barChartMakerMethod() { //https://www.youtube.com/watch?v=Bd76zMHdrDE
+        BarChart barChart = findViewById(R.id.barChartExplore);
+        BarDataSet barDataSet1 = new BarDataSet(NetIncome(), "Net");
+        barDataSet1.setColor(Color.RED);
+        BarDataSet barDataSet2 = new BarDataSet(Revenue(), "Rev");
+        barDataSet2.setColor(Color.GREEN);
+        BarDataSet barDataSet3 = new BarDataSet(EPS(), "EPS");
+        barDataSet3.setColor(Color.BLUE);
+        BarDataSet barDataSet4 = new BarDataSet(Dividend(), "DIV");
+        barDataSet4.setColor(Color.YELLOW);
+
+        BarData data = new BarData(barDataSet1, barDataSet2, barDataSet3, barDataSet4);
+        barChart.setData(data);
+
+        String[] years = new String[] {"2016", "2017", "2018", "2019", "2020"};
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(years));
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setPosition(XAxis.XAxisPosition.TOP_INSIDE);
+        xAxis.setGranularity(1);
+        xAxis.setGranularityEnabled(true);
+
+        //customize
+        barChart.getDescription().setEnabled(false); // disabled the bottom right corner description
+        barChart.setAutoScaleMinMaxEnabled(true);
+        barChart.setDrawValueAboveBar(true);
+          barChart
+
+        barChart.setDragEnabled(true);
+        barChart.setVisibleXRangeMaximum(3);
+
+        float barSpace = 0.08f;
+        float groupSpace = 0.38f;
+        data.setBarWidth(0.10f);  // these values * the number of columns have to equal 1
+
+        barChart.getXAxis().setAxisMinimum(0);
+        barChart.getXAxis().setAxisMaximum(0+barChart.getBarData().getGroupWidth(groupSpace, barSpace)*(NetIncome().size()));
+        //barChart.getAxisLeft().setAxisMinimum(0);
+
+        barChart.groupBars(0, groupSpace, barSpace);
+
+        barChart.invalidate();
+
 
 
     }
+    private ArrayList<BarEntry> NetIncome() {
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(1, -120));
+        barEntries.add(new BarEntry(2, 140));
+        barEntries.add(new BarEntry(3, 160));
+        barEntries.add(new BarEntry(4, 180));
+        barEntries.add(new BarEntry(5, 200));
+        return barEntries;
+    }
+    private ArrayList<BarEntry> Revenue() {
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(1, 320));
+        barEntries.add(new BarEntry(2, 340));
+        barEntries.add(new BarEntry(3, 460));
+        barEntries.add(new BarEntry(4, 580));
+        barEntries.add(new BarEntry(5, 600));
+        return barEntries;
+    }
+    private ArrayList<BarEntry> EPS() {
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(1, 1));
+        barEntries.add(new BarEntry(2, 2));
+        barEntries.add(new BarEntry(3, 4));
+        barEntries.add(new BarEntry(4, 5));
+        barEntries.add(new BarEntry(5, 8));
+        return barEntries;
+    }
+    private ArrayList<BarEntry> Dividend() {
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(1, 10));
+        barEntries.add(new BarEntry(2, 5));
+        barEntries.add(new BarEntry(3, 12));
+        barEntries.add(new BarEntry(4, 15));
+        barEntries.add(new BarEntry(5, 20));
+        return barEntries;
+    }
+
 
 
 
